7b78e0f574e2e6e12fa4d3cfd9fbe62ea9d0ba89
Farnoush Daliran <farnoushfdf@gmail.com>
2026-04-16 14:50:58 +0200

    add components and assets

---
+++ src/components/TravelList.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import travelPlansData from "../assets/travel-plans.json"
+
+const TravelList = () => {
+  const [travelPlans, setTravelPlans] = useState(travelPlansData);
+  console.log("*****", travelPlansData)
+
+
+
+  function handleDeletePlan(travelPlanId) {
+    const filteredPlans = setTravelPlans(travelPlans.filter(plan => plan.id !== travelPlanId) );
+    setTravelPlans(filteredPlans);
+  }
+
+  
+
+  // function handleDeletePlan(planId) {
+  //   console.log("plan deleted");
+  //   const filterData = dataState.filter((currentElement) => {
+  //     if (currentElement.id === planId) {
+  //       return false;
+  //     } else {
+  //       return true;
+  //     }
+  //   });
+  //   setDataState(filterData);
+  // }
+
+
+
+
+  return (
+    <div>
+            {travelPlans.map( (onePlan) => {
+                return(
+                    <div key={onePlan.id} className='plan-card'> 
+                      <img src={onePlan.image} alt="Travel Image" style={{width : "300px"}}/>
+                      <div>
+                         <h3>{onePlan.destination} ({onePlan.days} Days)</h3>
+                         <p>{onePlan.description}</p>
+                         <p><b>Price:</b> {onePlan.totalCost}</p>
+                         {onePlan.totalCost <= 350 && <p className='lable'><b>Great Deal</b></p>}
+                         {onePlan.totalCost >= 1500 && <p className='lable'><b>Premium</b></p>}
+                         <button onClick={() => handleDeletePlan(onePlan.id)}>Delete</button>
+                      </div>
+                   </div>
+                )
+            })}
+    </div>
+  )
+}
+
+export default TravelList
---
+++ src/components/TravelPlanCard.jsx
@@ -0,0 +1,8 @@
+
+const TravelPlanCard = ({plan}) => {
+  return (
+    <div>TravelPlanCard</div>
+  )
+}
+
+export default TravelPlanCard
